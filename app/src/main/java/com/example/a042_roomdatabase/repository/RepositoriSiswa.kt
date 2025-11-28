2cd2b2944baaceaa6bcc7ea164bdb8ce9a605756
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-28 15:43:10 +0700

    menambahkan interface repositorisiswa

--- app/src/main/java/com/example/a042_roomdatabase/repository/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a042_roomdatabase/repository/RepositoriSiswa.kt
@@ -1,2 +1,11 @@
 package com.example.a042_roomdatabase.repository
 
+import com.example.a042_roomdatabase.room.Siswa
+import com.example.a042_roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
