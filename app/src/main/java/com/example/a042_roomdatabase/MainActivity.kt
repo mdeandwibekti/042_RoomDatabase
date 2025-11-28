fdacce6d5388e404bdc9e5d5f1fac69e51f63eb0
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-28 19:51:30 +0700

    memperbaiki MainActivity.kt

--- app/src/main/java/com/example/a042_roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/a042_roomdatabase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a042_roomdatabase.ui.theme._042_RoomDatabaseTheme
+import com.example.a042_roomdatabase.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             _042_RoomDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
