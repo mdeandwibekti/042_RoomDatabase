cb34da5fb04226ef9b1407d9fc17462d9cb1e9f6
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-28 19:27:28 +0700

    menambah function creation pada PenyediaViewModel

--- app/src/main/java/com/example/a042_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a042_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -21,3 +21,7 @@ object PenyediaViewModel {
         }
     }
 }
+
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
