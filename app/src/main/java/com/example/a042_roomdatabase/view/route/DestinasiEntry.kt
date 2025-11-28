a3a32453a222a7b053cdce1cf5f33be297feffa2
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-28 19:53:40 +0700

    membuat DestinasiEntry.kt.kt

---
+++ app/src/main/java/com/example/a042_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.a042_roomdatabase.view.route
+
+import com.example.a042_roomdatabase.R
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
