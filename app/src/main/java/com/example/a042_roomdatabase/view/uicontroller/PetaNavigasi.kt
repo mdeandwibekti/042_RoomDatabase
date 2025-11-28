9c9c65c76d7d67bb7452c1dc8a0abd7d13965587
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-28 19:32:15 +0700

    menambah function hostnavigasi agar dapat memberi route pada halaman

--- app/src/main/java/com/example/a042_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a042_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -21,3 +21,26 @@ fun SiswaApp(
     HostNavigasi(navController = navController)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
