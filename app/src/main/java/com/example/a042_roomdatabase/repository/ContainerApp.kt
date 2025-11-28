b61d1ce613a877e9f51a010ebd4d9db45e05831d
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-28 15:51:42 +0700

    menambahkan interface dan class ContainerDataApp

---
+++ app/src/main/java/com/example/a042_roomdatabase/repository/ContainerApp.kt
@@ -0,0 +1,18 @@
+package com.example.a042_roomdatabase.repository
+
+import android.app.Application
+import android.content.Context
+import com.example.a042_roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+    }
+
