3615377a92eea73847bb536d755470f40090dcb8
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-05 18:31:10 +0700

    membuat val route untuk destinasi  edit

---
+++ app/src/main/java/com/example/a042_roomdatabase/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.a042_roomdatabase.view.route
+
+import com.example.a042_roomdatabase.R
+
+
+object DestinasiEditSiswa {
+    val route = "item_edit"
+     val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
