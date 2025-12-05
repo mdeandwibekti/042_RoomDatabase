6a240d880b16aa0782eb00b0993569b379ca8914
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-05 15:17:15 +0700

    membuat destinasidetailsiswa

---
+++ app/src/main/java/com/example/a042_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.a042_roomdatabase.view.route
+
+import com.example.a042_roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+}
